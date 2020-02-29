8b0c2eaa00735064f3a37c579e6c0c950eddab23
Alena Golovina <alenasgolovina@gmail.com>
2020-02-28 20:16:59 -0800

    facebook passport

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '195333228228836',
+        clientSecret: 'b3326c88ecffae090ffffd1b974b506f'
+    }
 }
